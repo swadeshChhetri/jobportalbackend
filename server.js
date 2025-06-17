9d27cce173af8f5ef8d5e8588aa27ba83e0159db
Swadesh Chhetri <163563708+swadeshChhetri@users.noreply.github.com>
2025-06-17 17:35:02 +0530

    allow the frontend to connect with backend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://jobportalcrud.vercel.app' ],
   credentials: true
 }));
 app.use(express.json());
